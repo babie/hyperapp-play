ca55a9bfc8e557dfa93b96ad6468c88dda41f71d
babie <babie.wildheart@gmail.com>
2018-12-19 14:08:39 +0900

    fix: Remove builded files

--- packages/create-hyperapp-play/dist/config.d.ts
+++
@@ -1,1 +0,0 @@
-declare const config = "\nmodule.exports = {\n  entry: {\n    app: 'play/app.js'\n  },\n  output: {\n    dir: '.play'\n  },\n  devServer: {\n    port: 5000,\n    hotOnly: true\n  }\n}\n";
--- packages/create-hyperapp-play/dist/config.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-var config = "\nmodule.exports = {\n  entry: {\n    app: 'play/app.js'\n  },\n  output: {\n    dir: '.play'\n  },\n  devServer: {\n    port: 5000,\n    hotOnly: true\n  }\n}\n";
