f07c402184689f863ee92eb2082410f5ac23927a
babie <babie.wildheart@gmail.com>
2018-12-19 16:39:59 +0900

    feat: Add program name to CLI

--- packages/create-hyperapp-play/src/index.ts
+++ packages/create-hyperapp-play/src/index.ts
@@ -2,13 +2,14 @@ import * as path from 'path'
 import * as cac from 'cac'
 import chalk from 'chalk'
 
-import { generateConfig } from './config'
+import { generateConfig } from './lib/config'
 
-const cli = cac()
+const cli = cac('create-hyperapp-play')
 cli
   .command('<project_root>', 'Integrate Hyperapp Play into your project')
   .option('--typescript', 'Generate TypeScript templates')
   .action(async (project_root, options) => {
+    // Generate config
     console.log(`${chalk.cyan('>')} generating play.config.js`)
     const dir = path.resolve(process.cwd(), project_root)
     await generateConfig(dir)
